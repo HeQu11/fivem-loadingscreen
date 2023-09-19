cea47445d1df0210a9d00ab7bbeed3d8109c0aa2
HeQu <32333860+HeQu11@users.noreply.github.com>
2023-09-19 20:36:12 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# fivem-loading
+I made this loading screen on request.
+
+Background video: https://www.youtube.com/watch?v=Ixv6kcHbpWE
+# fivem-loadingscreen
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version("cerulean")
+game("gta5")
+author("HeQu")
+
+files({
+	"index.html",
+	"style.css",
+	"assets/*",
+})
+
+loadscreen_cursor 'yes'
+loadscreen("index.html")
